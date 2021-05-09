2256ef9980337c44b1dce7af8e6d28f1bfb2eff5
wzl <1793268783@qq.com>
2021-05-10 03:39:34 +0800

    day07 mvc编写 commit

--- www/app.py
+++ www/app.py
@@ -4,8 +4,20 @@ logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
 from datetime import datetime
 from aiohttp import web
+'''
 def index(request):
 	return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+'''
+
+@get('/')
+def index(request):
+	users=yield from User.findAll()
+	return {
+		'__template__':'test.html',
+		'users':users
+	}
+
+
 
 async def init():
 	app=web.Application()
