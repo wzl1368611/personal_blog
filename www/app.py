f36cca1aba760171274d4e5548111df922f35d09
wzl <1793268783@qq.com>
2021-05-10 03:19:27 +0800

    day05 web框架封装aiohttp草稿版本 commit

--- www/app.py
+++ www/app.py
@@ -17,6 +17,13 @@ async def init():
 	# srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info('server started at http://127.0.0.1:9000...')
 	# return srv
+	#更改后的代码
+	app=web.Application(middlewares=[logger_factory,response_factory]) 
+	init_jinja2(app,filters=dict(datetime=datetime_filter))
+	add_routes(app,'handlers')
+	add_static(app)
+
+
 loop=asyncio.get_event_loop()
 loop.run_until_complete(init())
 loop.run_forever()
