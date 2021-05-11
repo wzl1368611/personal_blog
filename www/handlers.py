e80f1f35fd3c702c9c372e12a42b64b2eea6ede4
wzl <1793268783@qq.com>
2021-05-11 11:08:59 +0800

    day09 编写api commit

--- www/handlers.py
+++ www/handlers.py
@@ -17,3 +17,27 @@ async def index(request):
         '__template__':'index.html',
         'blogs':blogs
     }
+
+'''
+@get('/api/users')
+async def api_get_users(*,page='1'):
+	page_index=get_page_index(page)
+	num =await User.findNumber('count(id)')
+	p=Page(num,page_index)
+	if num==0:
+		return dict(page=p,users=())
+	users = await User.findAll(orderBy='created_at desc',limit=(p.offset,p.limit))
+	for u in users:
+		u.passwd='******'
+	return dict(page=p,users=users)
+'''
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
+
+
+
